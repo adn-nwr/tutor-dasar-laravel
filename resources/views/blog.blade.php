6ee4a5b862203d515fc90e9a38976fe0128c1a50
Adnan Anwar @Desktop <adnananwar85@gmail.com>
2025-04-30 16:33:20 +0800

    fitur article pada blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,6 +1,18 @@
 <x-layout title="Blog">
 
   <!-- Your content -->
-  <div>Ini adalah halaman Blog</div>
+
+  @foreach ($posts as $post)
+
+  <article class="py-4 max-w-screen-md border-b border-gray-300 mb-5">
+    <h2 class="mb-2 text-2xl tracking-tight font-bold text-gray-900"><a href="article/{{ $post['id'] }}" class="hover:underline">{{ $post['title'] }}</a></h2>
+    <div class="text-base text-gray-500">
+      {{ $post['author'] }} | {{ $post['created_date'] }}
+    </div>
+    <p class="my-4 font-light">{{ Str::limit($post['content'],80) }}</p>
+    <a href="article/{{ $post['id'] }}" class="font-medium font-stretch-50% text-blue-400 hover:underline">Read more &raquo;</a>
+  </article>
+
+  @endforeach
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,31 @@ Route::get('/', function () {
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', [
+        'posts' => [
+            ['id' => 1, 'title' => 'First Post', 'author' => 'Adnan Anwar', 'created_date' => '30 April 2025', 'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsam ullam eveniet nisi ducimus tenetur autem modi, repudiandae hic quos alias neque numquam ipsum facere, culpa earum doloremque inventore aliquid?'],
+            ['id' => 2, 'title' => 'Second Post', 'author' => 'Steven Spielberg', 'created_date' => '29 April 2025', 'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum est enim dolore praesentium temporibus sit inventore. Quas architecto deleniti quia voluptatum possimus dicta, adipisci explicabo ea, distinctio placeat optio asperiores.'],
+            ['id' => 3, 'title' => 'Third Post', 'author' => 'Thomas Dickens', 'created_date' => '28 April 2025', 'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam hic, aspernatur, ipsa, dolor sapiente labore sunt at sit dolore placeat consequatur quaerat! Ipsum nihil ab repellat cum voluptatibus ad voluptas.'],
+        ]
+    ]);
+});
+
+Route::get('/article/{id}', function ($id) {
+
+    $posts = [
+        'posts' => [
+            ['id' => 1, 'title' => 'First Post', 'author' => 'Adnan Anwar', 'created_date' => '30 April 2025', 'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsam ullam eveniet nisi ducimus tenetur autem modi, repudiandae hic quos alias neque numquam ipsum facere, culpa earum doloremque inventore aliquid?'],
+            ['id' => 2, 'title' => 'Second Post', 'author' => 'Steven Spielberg', 'created_date' => '29 April 2025', 'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum est enim dolore praesentium temporibus sit inventore. Quas architecto deleniti quia voluptatum possimus dicta, adipisci explicabo ea, distinctio placeat optio asperiores.'],
+            ['id' => 3, 'title' => 'Third Post', 'author' => 'Thomas Dickens', 'created_date' => '28 April 2025', 'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam hic, aspernatur, ipsa, dolor sapiente labore sunt at sit dolore placeat consequatur quaerat! Ipsum nihil ab repellat cum voluptatibus ad voluptas.'],
+        ]
+    ];
+
+    $post = collect($posts['posts'])->firstWhere('id', $id);
+    if (!$post) {
+        abort(404);
+    }
+
+    return view('article', ['post' => $post]);
 });
 
 Route::get('/about', function () {
