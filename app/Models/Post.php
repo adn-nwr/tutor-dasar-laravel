d26c0fa9ce2066a8d34bea9c6a9b742904a788f5
Adnan Anwar @Desktop <adnananwar85@gmail.com>
2025-05-05 11:43:18 +0800

    membuat model post eloquent

---
+++ app/Models/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    // public $id;
+    // public $title;
+    // public $author;
+    // public $created_date;
+    // public $content;
+
+    // public function __construct($id, $title, $author, $created_date, $content)
+    // {
+    //     $this->id = $id;
+    //     $this->title = $title;
+    //     $this->author = $author;
+    //     $this->created_date = $created_date;
+    //     $this->content = $content;
+    // }
+
+    // public static function getAll()
+    // {
+    //     return [
+    //         new Post(1, 'First Post', 'Adnan Anwar', '30 April 2025', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsam ullam eveniet nisi ducimus tenetur autem modi, repudiandae hic quos alias neque numquam ipsum facere, culpa earum doloremque inventore aliquid?'),
+    //         new Post(2, 'Second Post', 'Steven Spielberg', '29 April 2025', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum est enim dolore praesentium temporibus sit inventore. Quas architecto deleniti quia voluptatum possimus dicta, adipisci explicabo ea, distinctio placeat optio asperiores.'),
+    //         new Post(3, 'Third Post', 'Thomas Dickens', '28 April 2025', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam hic, aspernatur, ipsa, dolor sapiente labore sunt at sit dolore placeat consequatur quaerat! Ipsum nihil ab repellat cum voluptatibus ad voluptas.')
+    //     ];
+    // }
+
+    // public static function findById($id)
+    // {
+    //     $posts = self::getAll();
+    //     foreach ($posts as $post) {
+    //         if ($post->id == $id) {
+    //             return $post;
+    //         }
+    //     }
+    //     return null;
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,26 +8,12 @@ Route::get('/', function () {
 });
 
 Route::get('/blog', function () {
-    return view('blog', [
-        'posts' => [
-            ['id' => 1, 'title' => 'First Post', 'author' => 'Adnan Anwar', 'created_date' => '30 April 2025', 'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsam ullam eveniet nisi ducimus tenetur autem modi, repudiandae hic quos alias neque numquam ipsum facere, culpa earum doloremque inventore aliquid?'],
-            ['id' => 2, 'title' => 'Second Post', 'author' => 'Steven Spielberg', 'created_date' => '29 April 2025', 'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum est enim dolore praesentium temporibus sit inventore. Quas architecto deleniti quia voluptatum possimus dicta, adipisci explicabo ea, distinctio placeat optio asperiores.'],
-            ['id' => 3, 'title' => 'Third Post', 'author' => 'Thomas Dickens', 'created_date' => '28 April 2025', 'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam hic, aspernatur, ipsa, dolor sapiente labore sunt at sit dolore placeat consequatur quaerat! Ipsum nihil ab repellat cum voluptatibus ad voluptas.'],
-        ]
-    ]);
+    return view('blog', ['posts' => Post::all()]);
 });
 
 Route::get('/article/{id}', function ($id) {
 
-    $posts = [
-        'posts' => [
-            ['id' => 1, 'title' => 'First Post', 'author' => 'Adnan Anwar', 'created_date' => '30 April 2025', 'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ipsam ullam eveniet nisi ducimus tenetur autem modi, repudiandae hic quos alias neque numquam ipsum facere, culpa earum doloremque inventore aliquid?'],
-            ['id' => 2, 'title' => 'Second Post', 'author' => 'Steven Spielberg', 'created_date' => '29 April 2025', 'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum est enim dolore praesentium temporibus sit inventore. Quas architecto deleniti quia voluptatum possimus dicta, adipisci explicabo ea, distinctio placeat optio asperiores.'],
-            ['id' => 3, 'title' => 'Third Post', 'author' => 'Thomas Dickens', 'created_date' => '28 April 2025', 'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam hic, aspernatur, ipsa, dolor sapiente labore sunt at sit dolore placeat consequatur quaerat! Ipsum nihil ab repellat cum voluptatibus ad voluptas.'],
-        ]
-    ];
-
-    $post = collect($posts['posts'])->firstWhere('id', $id);
+    $post = Post::where('id', $id)->first();
     if (!$post) {
         abort(404);
     }
