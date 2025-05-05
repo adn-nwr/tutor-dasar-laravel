9516ef279d651cb92c3a1854bd997603687d0bab
Adnan Anwar @Desktop <adnananwar85@gmail.com>
2025-05-05 10:52:59 +0800

    membuat model Post

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,12 +1,12 @@
-<x-layout title="{{ $post['title'] }}">
+<x-layout title="{{ $post->title }}">
 
   <!-- Your content -->
 
   <article class="mb-5">
     <div class="text-base text-gray-500">
-      {{ $post['author'] }} | {{ $post['created_date'] }}
+      {{ $post->author }} | {{ $post->created_date }}
     </div>
-    <p class="my-4 font-light">{{ $post['content'] }}</p>
+    <p class="my-4 font-light">{{ $post->content }}</p>
   </article>
 
 </x-layout>
