e5598132baf1fde9015f68f23447d2bb18d33489
Adnan Anwar @Desktop <adnananwar85@gmail.com>
2025-05-20 15:22:06 +0800

    mengeluarkan seed user dan post dari database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,17 +12,5 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        $this->call([
-            PostSeeder::class,
-        ]);
-    }
+    public function run(): void {}
 }
